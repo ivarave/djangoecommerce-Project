9a5deb18e696f273d9e1dc851495d26b8d6f1884
Ivarave Emmanuel <eivarave404@gmail.com>
2025-06-10 12:34:20 +0100

    added DB password

--- ecom/settings.py
+++ ecom/settings.py
@@ -79,11 +79,11 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': 'yamabiko.proxy.rlwy.net',
+        'PORT': '32517',
     }
 }
 
