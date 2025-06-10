49313779cef91b9aef7105ca0fcf55e5517a9d4e
Ivarave Emmanuel <eivarave404@gmail.com>
2025-06-10 13:42:31 +0100

    whitnoise mistake corrected

--- ecom/settings.py
+++ ecom/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleWare.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     
 ]
 
