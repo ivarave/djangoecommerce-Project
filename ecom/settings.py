d7cf72b94a48dea531d8366c7cbe3e09adf689c9
Ivarave Emmanuel <eivarave404@gmail.com>
2025-06-11 10:15:41 +0100

    ironing it out 7

--- ecom/settings.py
+++ ecom/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-=$v^=@%y#c=@ko##_hs4up0p+%gpy279zksq#6b$(6vz+&h$#z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['djangoecommerce-production.up.railway.app', 'localhost', '127.0.0.1'  ]
+ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://djangoecommerce-project-production.up.railway.app']    
 
 
